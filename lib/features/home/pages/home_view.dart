fab94bf01c559e2dc57e49ef61e8c8786461e230
Abduqodir <abduqodirbahodirov@gmail.com>
2025-05-11 12:10:08 +0500

    sott_examen

--- lib/features/home/pages/home_view.dart
+++ lib/features/home/pages/home_view.dart
@@ -7,7 +7,6 @@ import 'package:sott_examen/features/home/widgets/home_grid_view.dart';
 import '../managers/home_bloc.dart';
 import '../managers/home_state.dart';
 import '../widgets/sott_bottom_navigation_bar.dart';
-import '../widgets/sott_icons_button.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -25,70 +24,53 @@ class HomeView extends StatelessWidget {
         if (state.status == HomeStatus.success) {
           return Scaffold(
             backgroundColor: Colors.white,
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              leading: Padding(
-                padding: EdgeInsets.only(left: 16.w),
-                child: Row(
-                  children: [
-                    Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        SvgPicture.asset("assets/logo.svg", width: 36.w, height: 36.h),
-                      ],
-                    ),
-                    SizedBox(width: 4.w),
-                    Text(
-                      "SOTT",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.w900,
-                      ),
+            body: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  toolbarHeight: 129.h,
+                  actions: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset("assets/icons/notification.svg"),
                     ),
                   ],
-                ),
-              ),
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset("assets/notifications.svg", width: 28.w, height: 28.h),
-                ),
-              ],
-              bottom: PreferredSize(
-                preferredSize: Size.fromHeight(60.h),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.h),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SottIconsButton(icon: "assets/icons/key.svg", title: "", width: 40, height: 40, callback: () {}),
-                      SottIconsButton(icon: "assets/icons/defend.svg", title: "", width: 40, height: 40, callback: () {}),
-                      SottIconsButton(icon: "assets/icons/clock.svg", title: "", width: 40, height: 40, callback: () {}),
-                      SottIconsButton(icon: "assets/icons/building.svg", title: "", width: 40, height: 40, callback: () {}),
-                    ],
+                  flexibleSpace: Image.asset(
+                    "assets/images/background.png",
+                    fit: BoxFit.cover,
+                  ),
+                  floating: true,
+                  snap: true,
+                  pinned: false,
+                  backgroundColor: Colors.white,
+                  title: Text(
+                    'SOTT',
+                    style: TextStyle(
+                      color: Color(0xFF1A1528),
+                      fontSize: 24.sp,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  leading: Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: SvgPicture.asset("assets/icons/logo.svg"),
+                  ),
+                  bottom: PreferredSize(
+                    preferredSize: Size.fromHeight(48.h),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          HomeBottomItems(image: "assets/icons/key.svg", title: "Purchase"),
+                          HomeBottomItems(image: "assets/icons/defend.svg", title: "Sale"),
+                          HomeBottomItems(image: "assets/icons/clock.svg", title: "Rent"),
+                          HomeBottomItems(image: "assets/icons/building.svg", title: "Pass"),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            body: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 20.w),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      HomeBottomItems(image: "assets/icons/key.svg", title: "Purchase"),
-                      HomeBottomItems(image: "assets/icons/defend.svg", title: "Sale"),
-                      HomeBottomItems(image: "assets/icons/clock.svg", title: "Rent"),
-                      HomeBottomItems(image: "assets/icons/building.svg", title: "Pass"),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: HomeGridView(state: state),
-                ),
+                HomeGridView(state: state),
               ],
             ),
             bottomNavigationBar: const SottBottomNavigationBar(),
